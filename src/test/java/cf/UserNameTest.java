df64f7ee7e0c8ee740e99836d7eb9f1c4e1b84ae
Debmalya Jash <debmalya.jash@gmail.com>
2016-03-12 00:26:26 +0800

    Added new class Username and related JUnit

---
+++ src/test/java/cf/UserNameTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package cf;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class UserNameTest {
+
+	@Test
+	public final void test() {
+		UserName userName = new UserName();
+		Assert.assertTrue(userName.canUpdate("Sheik", "Zelda", false, new String[]{"Link", "Ganon", "Zelda"}));
+		Assert.assertFalse(userName.canUpdate("CASTOR", "John", false, new String[]{"Castor","Polux","John"}));
+		Assert.assertTrue(userName.canUpdate("The_Boss", "the_boss", true, new String[]{"Snake","the_boss","Zero"}));
+		Assert.assertFalse(userName.canUpdate("Sheik", "Zelda", true, new String[]{"Link", "Ganon", "Zelda"}));
+		Assert.assertFalse(userName.canUpdate("CASTOR", "John", true, new String[]{"Castor","Polux","John"}));
+	}
+
+}
