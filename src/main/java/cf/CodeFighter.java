30fc781ab374a810b55a95b79d4c6cf37ac33f85
Debmalya Jash <debmalya.jash@gmail.com>
2016-06-11 20:02:54 +0800

    Update CodeFighter.java

--- src/main/java/cf/CodeFighter.java
+++ src/main/java/cf/CodeFighter.java
@@ -197,4 +197,85 @@ public class CodeFighter {
         return r;
 
     }
+    
+    /**
+	 * An arbitrage is the simultaneous purchase and sale of an asset in order
+	 * to profit from a difference in the price. This type of trade exploits
+	 * price differences between similar or identical financial instruments,
+	 * either on different markets or in different forms. You are a currency
+	 * trader looking for arbitrage opportunities in the currency market using
+	 * these three quotes:
+	 * 
+	 * 1 . The cost of USD per EUR (USD/EUR) for converting USD -> EUR. 2 . The
+	 * cost of EUR per GBP (EUR/GBP) for converting EUR -> GBP. 3 . The cost of
+	 * GBP per USD (GBP/USD) for converting GBP -> USD.
+	 * 
+	 * You must use your USD to buy EUR, then use your EUR to buy GBP, and
+	 * finally use your GBP to buy USD, resulting in some sort of profit or loss
+	 * (i.e., USD->EUR->GBP->USD). Reverse trading is not allowed, so you are
+	 * limited to the 3 exchanges in the direction shown above. For example, you
+	 * can convert USD->EUR, which means selling US Dollars and buying Euros;
+	 * you cannot invert the fraction to sell Euros and buy US Dollars. Given
+	 * 100,000 USD for each trade, calculate the arbitrage profit truncated to
+	 * whole dollars (USD); otherwise, print if there is no arbitrage
+	 * opportunity.
+	 * 
+	 * Input format The first line contains a single integer, N, denoting the
+	 * number of quotes. Each of the N subsequent lines describes a quote in the
+	 * form of three space-separated integers: 1. The first quote is a real
+	 * number denoting the price quote for (USD/EUR). 2. The second quote is a
+	 * real number denoting the price quote for (EUR/GBP). 3. The third quote is
+	 * a real number denoting the price quote for (GBP/USD).
+	 * 
+	 * Constraints 1 <= N <= 100 0.001 < quotes <= 1000
+	 * 
+	 * Output Format For each trade, print a single line denoting the arbitrage
+	 * profit for that trade; if no arbitrage opportunity exists, print 0. You
+	 * should have a total of N lines of output.
+	 * 
+	 * Sample Input 2 1.1837 1.3829 0.6102 1.1234 1.2134 1.2311
+	 * 
+	 * Sample Output 114 0
+	 * 
+	 * 
+	 * Explanation There are 2 test cases. N = 2.
+	 * 
+	 * 1. You use your 100,000 USD to buy 84,480.8651 EUR. You then use your
+	 * 84,480.8651 EUR to buy 61,089.6414 GBP. Finally, you use your 61,089.6414
+	 * GBP to buy USD. Because we started out with 100,000 USD, our net profit
+	 * in whole dollars is 114 USD. 2. There is no arbitrage opportunity here
+	 * (the conversion would end up losing money), so we print 0.
+	 * 
+	 * 
+	 * 
+	 */
+	public static void calculateArbritage() {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int t = scanner.nextInt();
+			int[] r = new int[t];
+			for (int i = 0; i < t; i++) {
+				Double usdToEur = scanner.nextDouble();
+				Double eurToGbp = scanner.nextDouble();
+				Double gbpToUsd = scanner.nextDouble();
+
+				Double capital = 100000.00d;
+
+				Double eur = capital / usdToEur;
+				Double gbp = eur / eurToGbp;
+				Double usd = gbp / gbpToUsd;
+
+				double arbritage = usd - capital;
+				if (arbritage > 0.00d) {
+					r[i] = (int) (Math.round(arbritage * 10000) / 10000);
+				} else {
+					r[i] = 0;
+				}
+			}
+
+			for (int i = 0; i < t; i++) {
+				System.out.println(r[i]);
+			}
+		}
+	}
+
 }
