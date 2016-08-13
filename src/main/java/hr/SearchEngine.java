4fd01bd2878ea9740c475e82601d85022e25ccb4
debmalyajash <debmalya.jash@gmail.com>
2016-08-13 15:35:43 +0800

    Added new source

---
+++ src/main/java/hr/SearchEngine.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2015-2016 Debmalya Jash
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package hr;
+
+/**
+ * 
+ * Build a search engine to help users to find destinations around the world
+ * based on their passions. Search engine can only receive as argument a list of
+ * passions like 'Surfing', 'Shark Diving', 'Culture' and should recommend a
+ * list of destinations that match those passions.
+ * 
+ * To implement such search engine we need to have a database , destination,
+ * passions, endorsement count. How would implement such search engine?
+ * 
+ * What is the best way to do a combined search ? (e.g. If user search for
+ * Architecture, Wine what is the best way to combine those two?) Is summing
+ * them enough? Are you familiar with 'popularity bias' effect? How you overcome
+ * that? How your search engine will consider those hidden gems and show them as
+ * part or your result?
+ * 
+ * Some travelers endorse with some jokes. How do you ignore those?
+ * What else you need to think about to make search engine results relevant to the users?
+ * 
+ * 
+ * @author debmalyajash
+ *
+ */
+public class SearchEngine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
