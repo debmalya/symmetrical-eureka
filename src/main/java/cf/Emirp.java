64de1e4d291ca2116341dccc4a3650791fa5e50d
debmalyajash <debmalya.jash@gmail.com>
2016-08-16 13:07:55 +0800

    Added Emirp

---
+++ src/main/java/cf/Emirp.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2015-2016 Debmalya Jash
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cf;
+
+/**
+ * 
+ * Emirp is prime spelled backwards. A number is emirp if both the number itself
+ * and the same number written backwards are prime. For instance, 13 and 31 both
+ * are prime.
+ * 
+ * But 23 is not, since 32 is a composite number.
+ * 
+ * Emirps are hiding in a large integer num, waiting for the next skilled
+ * programmer to find them! They are clustered together, so an emirp can be
+ * formed from a set of consecutive digits of num.
+ * 
+ * @author debmalyajash
+ *
+ */
+public class Emirp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Find the sum of all emirps in num. If none can be found, return -1 instead.
+	 * @param num to be checked for Emirp.
+	 * @return the sum, if there is any 
+	 */
+	int emirpMinion(int num) {
+		return -1;
+	}
+
+}
