9eb6699c295fdbc4a345c033450903372ac72a33
debmalyajash <debmalya.jash@gmail.com>
2016-09-01 11:15:37 +0800

    Added new class CircumscribedCircle

---
+++ src/main/java/cf/CircumscribedCircle.java
@@ -0,0 +1,27 @@
+package cf;
+
+/**
+ * 
+ * @author debmalyajash
+ *
+ */
+public class CircumscribedCircle {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	/**
+	 * 
+	 * @param triangle
+	 *            - three coordinates of a triangle (x,y).
+	 * @return - find the (x,y) coordinates of the circumscribed circle and its
+	 *         radius.
+	 *         
+//	         TODO  to be implemented
+	 */
+	int[] circumcircle(int[][] triangle) {
+		return null;
+	}
+}
---
+++ src/test/java/cf/CircumscribedCircleTest.java
@@ -0,0 +1,44 @@
+package cf;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CircumscribedCircleTest {
+
+	@Test
+	public void testCircumcircle() {
+		CircumscribedCircle circle = new CircumscribedCircle();
+		int[][] triangle = new int[][] { { 3, 2 }, { 1, 4 }, { 5, 4 } };
+		int[] expecteds = new int[] { 3, 4, 2 };
+		Assert.assertArrayEquals(expecteds, circle.circumcircle(triangle));
+
+		triangle = new int[][] { { 8, 6 }, { 8, -2 }, { 2, -2 } };
+		expecteds = new int[] { 5, 2, 5 };
+		Assert.assertArrayEquals(expecteds, circle.circumcircle(triangle));
+		
+		triangle = new int[][] { { 5, 7 }, { 6, 6 }, { 2, -2 } };
+		expecteds = new int[] { 2, 3, 5 };
+		Assert.assertArrayEquals(expecteds, circle.circumcircle(triangle));
+		
+		triangle = new int[][] { { 3, 4 }, { 4, 5 }, { 4, 3 } };
+		expecteds = new int[] { 4, 4, 1 };
+		Assert.assertArrayEquals(expecteds, circle.circumcircle(triangle));
+		
+		triangle = new int[][] { { 3, 4 }, { 4, 5 }, { 4, 3 } };
+		expecteds = new int[] { 4, 4, 1 };
+		Assert.assertArrayEquals(expecteds, circle.circumcircle(triangle));
+		
+		triangle = new int[][] { { 2, 11 }, { 6, 7 }, { -2, 7 } };
+		expecteds = new int[] { 2, 7, 4 };
+		Assert.assertArrayEquals(expecteds, circle.circumcircle(triangle));
+		
+		triangle = new int[][] { { 5, 1 }, { -2, 0 }, { 4, 8 } };
+		expecteds = new int[] { 1, 4, 5 };
+		Assert.assertArrayEquals(expecteds, circle.circumcircle(triangle));
+		
+		triangle = new int[][] { { 1, 4 }, { -2, 3 }, { 5, 2 } };
+		expecteds = new int[] { 1, -1, 5 };
+		Assert.assertArrayEquals(expecteds, circle.circumcircle(triangle));
+	}
+
+}
