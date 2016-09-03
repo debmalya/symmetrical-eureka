0bddb1e1c16b1ebbdc8434b3d779bc4f6b3dda48
debmalyajash <debmalya.jash@gmail.com>
2016-09-03 17:18:11 +0800

    Adding offset

---
+++ src/main/java/cf/Offset.java
@@ -0,0 +1,19 @@
+package cf;
+
+public class Offset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * 
+	 * @param chain
+	 * @return
+	 */
+	int offsets(String chain) {
+		return 0;
+	}
+
+}
