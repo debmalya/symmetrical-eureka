f50d09b6c8862413ca6bf8d0d6e4c8b2b2c2a1cc
debmalyajash <debmalya.jash@gmail.com>
2016-09-17 07:36:21 +0800

    Added Gravity Tree

--- src/main/java/hr/GravityTree.java
+++ src/main/java/hr/GravityTree.java
@@ -15,13 +15,38 @@
  */
 package hr;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
+// It's a tree where each vertex is numbered from 1 to n and vertex 1
+// always
+// denotes the root. Each edge has a length of 1, so a node has a
+// distance of 1 from its parent, a distance of 2 from its grandparent,
+// and
+// so on.
+
+// Some vertex, v , can be turned on, which results in all vertices
+// (including ) of the subtree rooted at beginning to attract the tree's
+// other vertices.
+
+// The attractive forces exerted on some vertex, u, is equal to the
+// summation of squared distances between vertex u and all vertices that
+// are currently switched on.
+
+// For example, if two nodes are switched on that have distances of 1
+// and 2
+// from node u, then the forces exerted on would be 1**2 + 2**2 = 1 + 4
+// = 5.
 /**
  * @author debmalyajash
  *
  */
 public class GravityTree {
+
+	private static LinkedHashMap<Integer, Integer> childMap = new LinkedHashMap<>();
 
 	/**
 	 * @param args
@@ -40,6 +65,7 @@ public class GravityTree {
 				arr[i] = in.nextInt();
 			}
 
+			Map<Integer, List<Integer>> childMap = createTree(arr);
 			// The next line contains an integer, q , denoting the number of
 			// experiments she plans to perform.
 			int q = in.nextInt();
@@ -49,51 +75,128 @@ public class GravityTree {
 			// experiment.
 			int[] u = new int[q];
 			int[] v = new int[q];
+			int[] r = new int[q];
 			for (int i = 0; i < q; i++) {
 				u[i] = in.nextInt();
 				v[i] = in.nextInt();
+				r[i] = getResult(childMap, u[i], v[i]);
 			}
+
+			for (int i = 0; i < q; i++) {
+				System.out.println(r[i]);
+			}
+
 		}
 
 	}
 
 	/**
-	 * Create a Tree from the passed array of edge information.
-	 * @param arr
+	 * @param gravityMap
+	 *            - Map containing tree.
+	 * @param u
+	 * @param v
 	 * @return
 	 */
-	public MyTree createTree(int[] arr){
-		MyTree m = new MyTree();
-		return m;
+	public static int getResult(Map<Integer, List<Integer>> gravityMap, int u, int v) {
+		// get all the children of u
+
+		if (v == 1) {
+			// It is from root every node will be considered.
+			return calculateChildForce(gravityMap, u, v) + calculateParentForce(gravityMap, u, v);
+		} else {
+			int[] nodes = turnOn(gravityMap,v);
+			return 0;
+		}
 	}
-	/**
-	 * 
-	 * @param vertexTurnedOn - v.
-	 * @param vertexObserving - u.
-	 * @return the attractive force on vertex u.
+
+	/**If v is turned on, which nodes will be considered.
+	 * @param gravityMap - contains all child nodes and parents.
+	 * @param v - node to be turned on.
+	 * @return - Nodes to be considered.
 	 */
-	public int getTheForce(int vertexTurnedOn, int vertexObserving){
-		return 0;
+	private static int[] turnOn(Map<Integer, List<Integer>> gravityMap, int v) {
+		// TODO Auto-generated method stub
+		return null;
 	}
-	
-	
-	public static class MyTree {
-		// It's a tree where each vertex is numbered from 1 to n and vertex 1
-		// always
-		// denotes the root. Each edge has a length of 1, so a node has a
-		// distance of 1 from its parent, a distance of 2 from its grandparent,
-		// and
-		// so on.
 
-		// Some vertex, v , can be turned on, which results in all vertices
-		// (including ) of the subtree rooted at beginning to attract the tree's
-		// other vertices.
+	/**
+	 * @param gravityMap
+	 * @param u
+	 * @param v
+	 * @return
+	 */
+	private static int calculateParentForce(Map<Integer, List<Integer>> gravityMap, int u, int v) {
+		int distance = 0;
+		while (true) {
+			Integer value = childMap.get(u);
+			if (value != null) {
+				if (childMap.get(u) == v) {
+					distance = 1;
+					break;
+				} else {
+					distance++;
+					u = value;
+				}
 
-		// The attractive forces exerted on some vertex, u, is equal to the
-		// summation of squared distances between vertex u and all vertices that
-		// are currently switched on.
-
-		// For example, if two nodes are switched on that have distances of 1 and 2
-		// from node u, then the forces exerted on would be 1**2 + 2**2 = 1 + 4 = 5.
+			} else {
+				// root node
+				break;
+			}
+		}
+		return (int) Math.pow(distance, 2);
 	}
+
+	/**
+	 * @param gravityMap
+	 * @param u
+	 * @param f
+	 * @return
+	 */
+	public static int calculateChildForce(Map<Integer, List<Integer>> gravityMap, int u, int v) {
+		List<Integer> urChildren = gravityMap.get(u);
+		List<Integer> children = new ArrayList<>();
+		children.addAll(urChildren);
+
+		// all are children at a distance of 1.
+		int f = children.size();
+
+		// Continue for grand children and so on.
+		List<Integer> grandChildren = new ArrayList<>();
+
+		int distance = 2;
+		while (!children.isEmpty()) {
+			for (int i = 0; i < children.size(); i++) {
+
+				List<Integer> currentChild = gravityMap.get(children.get(i));
+				if (currentChild != null && !currentChild.isEmpty()) {
+					grandChildren.addAll(currentChild);
+					f += Math.pow(distance, 2);
+				}
+				if (i == children.size() - 1) {
+					children.clear();
+					children.addAll(grandChildren);
+					grandChildren.clear();
+					distance++;
+				}
+			}
+		}
+		return f;
+	}
+
+	public static Map<Integer, List<Integer>> createTree(int[] arr) {
+		LinkedHashMap<Integer, List<Integer>> gravityMap = new LinkedHashMap<>();
+
+		for (int i = 0; i < arr.length; i++) {
+			int currentNode = i + 2;
+			List<Integer> childList = gravityMap.get(arr[i]);
+			if (childList == null) {
+				childList = new ArrayList<>();
+			}
+			childList.add(currentNode);
+			gravityMap.put(arr[i], childList);
+			childMap.put(currentNode, arr[i]);
+		}
+		return gravityMap;
+	}
+
 }
