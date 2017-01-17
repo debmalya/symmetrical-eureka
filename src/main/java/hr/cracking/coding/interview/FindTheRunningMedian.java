d573ffb9757125cf8aefc989338b74d95c2f9cbf
debmalyajash <debmalya.jash@gmail.com>
2017-01-17 17:45:07 +0800

    Find The Running Median

--- src/main/java/hr/cracking/coding/interview/FindTheRunningMedian.java
+++ src/main/java/hr/cracking/coding/interview/FindTheRunningMedian.java
@@ -25,28 +25,28 @@ import java.util.Scanner;
  *
  */
 public class FindTheRunningMedian {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try (Scanner in = new Scanner(System.in)){
-			int n = in.nextInt();
-			
-			List<Integer> values = new ArrayList<>();
-			for (int i = 0; i < n ; i ++) {
-				values.add(in.nextInt());
-				Collections.sort(values);
-				int mid = i/2;
-				if (values.size() % 2 == 1){
-					System.out.println(String.format("%.1f", (double)values.get(mid)));
-				} else {
-					
-					System.out.println(String.format("%.1f", (double)(values.get(mid)+values.get(mid+1))/2.0));
-				}
-			}
+	
+//	Learnt from https://www.hackerrank.com/lahouari
+	 public static double median(List<Integer> data) {
+		  if ((data.size() & 1) == 1) {
+		    return data.get(data.size()/2);
+		  } else {
+		    int midSize = data.size()/2;
+		    return (data.get(midSize-1) + data.get(midSize)) / 2.0;
+		  }
 		}
 
-	}
+		public static void main(String[] args) {
+		  Scanner in = new Scanner(System.in);
+		  int n = in.nextInt();
+		  List<Integer> data = new ArrayList<>();
+		  for (int i = 0; i < n; i++) {
+		    int a_i = in.nextInt();
+		    int pos = Collections.binarySearch(data, a_i);
+		    if (pos < 0) pos = Math.abs(pos)-1;
+		    data.add(pos, a_i);
+		    System.out.format("%1$.1f\n", median(data));
+		  }
+		}
 
 }
