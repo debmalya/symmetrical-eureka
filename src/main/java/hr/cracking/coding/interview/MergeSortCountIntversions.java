4caba0a4583f4520f3510427a015f81c6e500fa3
Debmalya Jash <debmalya.jash@gmail.com>
2017-01-22 07:50:51 +0800

    Cracking Merge Sort

--- src/main/java/hr/cracking/coding/interview/MergeSortCountIntversions.java
+++ src/main/java/hr/cracking/coding/interview/MergeSortCountIntversions.java
@@ -29,7 +29,7 @@ public class MergeSortCountIntversions {
 	public static void main(String[] args) {
 		try (Scanner in = new Scanner(System.in)) {
 			int d = in.nextInt();
-			int[] result = new int[d];
+			long[] result = new long[d];
 
 			for (int i = 0; i < d; i++) {
 				int n = in.nextInt();
@@ -39,12 +39,77 @@ public class MergeSortCountIntversions {
 					arr[j] = in.nextInt();
 				}
 
-				result[i] = getSwapCount(arr);
+				result[i] = countInversions(arr);
 
+			}
+			
+			for (int i = 0; i < d; i++) {
+				System.out.println(result[i]);
 			}
 		}
 
 	}
+	
+	/**
+	 * 
+	 * @param arr
+	 * @return
+	 */
+	private static long countInversions(int[] arr) {
+        int[] aux = arr.clone();
+        return countInversions(arr, 0, arr.length - 1, aux);
+    }
+	
+	/**
+	 * Divide and conquer. Divide the array in the middle. Count the inversions. 
+	 * Merge the array count inversions.
+	 * 
+	 * @param arr for whom inversion counts will be calculated.
+	 * @param lo low index.
+	 * @param hi high index.
+	 * @param aux auxiliary array.
+	 * @return total number of inversion counts.
+	 */
+	  private static long countInversions(int[] arr, int lo, int hi, int[] aux) {
+	        if (lo >= hi) return 0;
+
+	        int mid = lo + (hi - lo) / 2;
+
+	        long count = 0;
+	        count += countInversions(aux, lo, mid, arr);
+	        count += countInversions(aux, mid + 1, hi, arr);
+	        count += merge(arr, lo, mid, hi, aux);
+
+	        return count;
+	    }
+	  
+	  /**
+	   * Merge the arrays arr and aux, return the inversion count.
+	   * @param arr 
+	   * @param lo low index.
+	   * @param mid middle index.
+	   * @param hi high index.
+	   * @param aux auxiliary array.
+	   * @return number of inversions.
+	   */
+	  private static long merge(int[] arr, int lo, int mid, int hi, int[] aux) {
+	        long count = 0;
+	        int i = lo, j = mid + 1, k = lo;
+	        while (i <= mid || j <= hi) {
+	            if (i > mid) {
+	                arr[k++] = aux[j++];
+	            } else if (j > hi) {
+	                arr[k++] = aux[i++];
+	            } else if (aux[i] <= aux[j]) {
+	                arr[k++] = aux[i++];
+	            } else {
+	                arr[k++] = aux[j++];
+	                count += mid + 1 - i;
+	            }
+	        }
+
+	        return count;
+	    }
 
 	/**
 	 * @param arr
