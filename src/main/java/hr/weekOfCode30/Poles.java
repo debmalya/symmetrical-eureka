519ce0d2fa0620d0efd1c308f052f75cb3da4a45
debmalyajash <debmalya.jash@gmail.com>
2017-03-17 11:59:00 +0800

    Week of code 30

---
+++ src/main/java/hr/weekOfCode30/Poles.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2015-2016 Debmalya Jash
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package hr.weekOfCode30;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class Poles {
+
+	/**
+	 * There are n telephone poles ascending a mountain and each pole has a
+	 * weight and a unique altitude.Our program must move the poles into k
+	 * number of stacks, but we can only rearrange the poles according to
+	 * certain criteria:
+	 * 
+	 * 1. Poles can only be moved from higher altitudes to lower altitudes.
+	 * 
+	 * 2. Stacks can only be formed at the initial pole altitudes.
+	 * 
+	 * 3. A stack can consist of at least one pole.
+	 * 
+	 * Moving the poles down the mountain also costs money. Moving a pole with
+	 * weight w and altitude x1 to an altitude x2 of where (x1 > x2)  costs w * (x1 - x2)  .
+	 * 
+	 * Determine the least amount of money needed to rearrange the poles into stacks.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
