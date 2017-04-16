6316d1e55c493eb236a432a3c51af9f61c415695
debmalyajash <debmalya.jash@gmail.com>
2017-04-16 08:14:41 +0800

    Drawing Book

---
+++ src/main/java/hr/DrawingBook.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2015-2016 Debmalya Jash
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package hr;
+
+import java.util.Scanner;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class DrawingBook {
+
+	public static int solve(int n, int p) {
+		int fromBack = n - p;
+		int fromFront = p / 2;
+		if (n % 2 == 1) {
+			fromBack--;
+		}
+		if (fromBack > 1) {
+			fromBack /= 2;
+		}
+		int result = (fromBack < fromFront) ? fromBack : fromFront;
+		return result;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try (Scanner in = new Scanner(System.in)) {
+			int n = in.nextInt();
+			int p = in.nextInt();
+			int result = solve(n, p);
+			System.out.println(result);
+		}
+	}
+
+}
---
+++ src/test/java/hr/DrawingBookTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2015-2016 Debmalya Jash
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package hr;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class DrawingBookTest {
+
+	/**
+	 * Test method for {@link hr.DrawingBook#solve(int, int)}.
+	 */
+	@Test
+	public void testSolve() {
+		Assert.assertEquals(0, DrawingBook.solve(6, 1));
+		Assert.assertEquals(0, DrawingBook.solve(5, 4));
+		Assert.assertEquals(1, DrawingBook.solve(6, 4));
+		Assert.assertEquals(1, DrawingBook.solve(6, 3));
+		Assert.assertEquals(1, DrawingBook.solve(6, 2));
+		Assert.assertEquals(0, DrawingBook.solve(6, 1));
+		Assert.assertEquals(0, DrawingBook.solve(6, 6));
+		Assert.assertEquals(1, DrawingBook.solve(6, 5));
+	}
+
+}
